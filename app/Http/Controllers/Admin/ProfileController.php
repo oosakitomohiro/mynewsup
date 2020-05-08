4a241003e4c4d9a93d63b8dad3755b4635ac3953
大崎　智広 <t.oosaki.tb@gmail.com>
2020-05-08 12:43:37 +0000

    投稿データ保存

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -11,8 +12,13 @@ class ProfileController extends Controller
     {
         return view('admin.profile.create');
     }
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
+        $profile->fill($form);
+        $profile->save();
         return redirect('admin/profile/create');
     }
     public function edit()
@@ -23,4 +29,6 @@ class ProfileController extends Controller
     {
         return redirect('admin/profile/edit');
     }
+   
+ 
 }
--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -15,13 +15,13 @@
                     </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2">タイトル</label>
+                        <label class="col-md-2" for="title">タイトル</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title')}}">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">本文</label>
+                        <label class="col-md-2" for="body">本文</label>
                         <div class="col-md-10">
                             <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
                         </div>
